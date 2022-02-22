5ed0efd6fec0e09aa9592b8810272d6eb0150dd8
Esben <esben1998@hotmail.com>
2022-02-22 12:31:42 +0100

    Solution to InputOutput A-C

---
+++ homework_exercises/InputOutput/B/main.cs
@@ -0,0 +1,13 @@
+using static System.Console;
+using static System.Math;
+using System;
+
+class main {
+
+	public static void Main(string[] args) {
+			foreach(var arg in args) {
+				double x = double.Parse(arg);
+					WriteLine($"{x} {Sin(x)} {Cos(x)}");
+			}
+		}
+	}
